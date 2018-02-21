b959470388e0ac093fb34a5578b39f2e060ee193
Estudiantes <Estudiantes@Labing70212.udistrital.edu.co>
2018-02-21 17:58:17 -0500

    Parqueadero.01

---
+++ Vehiculos/src/vehiculos/Moto.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vehiculos;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class Moto extends Vehiculo {
+    
+   
+        
+        
+        void acelerar() {
+        
+         System.out.println("la moto está acelerando");
+        
+    }
+        void frenar (){
+         System.out.println("la moto está Frenando");
+        
+    }
+        void derrapar(){
+         System.out.println("la moto está derrapando");
+        
+    }
+    }
+    
+
---
+++ Vehiculos/src/vehiculos/Vehiculo.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vehiculos;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class Vehiculo {
+
+    /**
+     *
+     */
+    public string marca;
+    private int cilindraje;
+    private string modelo;
+    private int año;
+
+    void acelerar() {
+        
+        System.out.println("el carro está acelerando");
+        
+    }
+
+    void frenar() {
+ System.out.println("el carro está frenando");
+    }
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+    private static class string {
+
+        public string() {
+        }
+    }
+
+}
